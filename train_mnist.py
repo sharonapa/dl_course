1008ac41c04e6c453739ee20712f12973bd11704
sharonapa <sharon.napa@gmail.com>
2021-03-31 23:41:17 +0300

    Add files via upload

--- train_mnist.py
+++ train_mnist.py
@@ -1,6 +1,9 @@
+import random
 import tensorflow as tf
+from matplotlib import cm
 
 from my_network import L_layer_model
+import matplotlib.pyplot as plt
 
 
 def get_mnist():
@@ -27,14 +30,37 @@ def get_mnist():
 
 
 def train_mnist():
-    (x_train, y_train), (x_test, y_test) = get_mnist()
-    X = x_train
-    Y = y_train
-    layers_dims = [X.shape[1], 20, 7, 5, 10]
+    random.seed(123)
 
-    X = X.T
-    Y = Y.T
-    w_b_parmas_dic, costs = L_layer_model(X, Y, layers_dims, learning_rate=0.009, num_iterations=100, batch_size=4,
+    #(x_train, y_train), (x_test, y_test) = get_mnist()
+    # X = x_train
+    # Y = y_train
+    # X = X.T
+    # Y = Y.T
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+    x_train = x_train.reshape(x_train.shape[0], 28 * 28)  # 784
+    x_test = x_test.reshape(x_test.shape[0], 28 * 28)
+
+    x_train = x_train.astype('float64')
+    x_test = x_test.astype('float64')
+
+    x_train = x_train / 255
+    x_test = x_test / 255
+
+    y_train = y_train.reshape(x_train.shape[0], 1)
+    y_test = y_test.reshape(x_test.shape[0], 1)
+
+    num_classes = 10
+    y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+    y_test = tf.keras.utils.to_categorical(y_test, num_classes)
+
+    plt.title((y_train[0]))
+    plt.imshow(x_train[0].reshape(28, 28), cmap=cm.binary)
+
+
+    layers_dims = [784, 20, 7, 5, 10]
+    w_b_parmas_dic, costs = L_layer_model(x_train, y_train, layers_dims, learning_rate=0.009, num_iterations=50000, batch_size=100,
                                           use_batchnorm=False)
     print("costs: ", costs)
 
